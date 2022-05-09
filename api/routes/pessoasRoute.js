5e1eeb140996b05ed936a33fbfe61b6caa5a62cc
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-09 00:02:59 -0300

    feat: added nova rota get  peessoas:id

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,9 +1,10 @@
-const express = require('express')
 const { Router } = require('express')
 const PessoaController = require ('../controllers/PessoaController')
 
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegarTodasAsPessoas)
+
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 
 module.exports = router
