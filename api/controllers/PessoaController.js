fcf3bcb2b9bb5953ce27409dc5611064644f334d
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-10 09:59:53 -0300

    feat: adicionando novo método para consulta de ativos

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,14 +1,23 @@
 const database = require('../models')
 
 class PessoaController {
-    static async pegarTodasAsPessoas(req, res) {
+    static async pegaPessoasAtivas(req, res) {
        try {
-        const todasAsPessoas = await database.Pessoas.findAll()
-        return res.status(200).json(todasAsPessoas)
-       }  catch (error) {
+        const pessoasAtivas = await database.Pessoas.findAll()
+        return res.status(200).json(pessoasAtivas)
+       } catch (error) {
            return res.status(500).json(error.message)
        }      
     }
+
+    static async pegarTodasAsPessoas(req, res) {
+      try {
+       const todasAsPessoas = await database.Pessoas.scope('todos').findAll()
+       return res.status(200).json(todasAsPessoas)
+      } catch (error) {
+          return res.status(500).json(error.message)
+      }      
+   }
 
     static async pegaUmaPessoa(req, res) {
         const { id } = req.params 
@@ -51,9 +60,19 @@ class PessoaController {
         try {
             await database.Pessoas.destroy( { where: { id: Number(id) }})
             return res.status(200).json({ mensagem: `id ${id} deletado` })           
-        } catch (error){
+        } catch (error) {
             return res.status(500).json(error.message)
         }
+    }
+
+    static async restauraPessoa(req, res) {
+        const { id } = req.params 
+        try {
+          await database.Pessoas.restore( { where: { id: Number(id) }})
+          return res.status(200).json({ mensagem: `id ${id} restaurado` }) 
+        } catch (error) {
+          return res.status(500).json(error.message)
+      }
     }
 
     static async pegaUmaMatricula(req, res) {
